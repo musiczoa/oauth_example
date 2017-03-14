27d0d338688850c5e1e618af8a5699ee9e65f7ef
Hwa <musiczoa@naver.com>
2017-03-14 23:35:25 +0900

    omniauth 정함

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,11 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  OmniAuth.config.full_host = Rails.env.production? ? 'https://oauth-example.herokuapp.com' : 'http://localhost:3000'
   provider :google_oauth2, 'client_id', 'client_secret',
            {
-               :scope => ['email', 'profile', 'https://www.googleapis.com/auth/androidpublisher'],
+               :scope => ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/androidpublisher'],
                :access_type => 'offline'
            }
   # OmniAuth.config.on_failure = SessionsController.action(:oauth_failure)
-  OmniAuth.config.full_host = Rails.env.production? ? 'https://oauth-example.herokuapp.com' : 'http://localhost:3000'
 end

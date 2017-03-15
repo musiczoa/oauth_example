f452ee8f270ba601d0d85261a9c727dce8d88a51
Hwa <musiczoa@naver.com>
2017-03-15 13:08:19 +0900

    수정중

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
+
   get 'signout', to: 'sessions#destroy', as: 'signout'
+
   get 'payload', to: 'home#make_developer_payload', as: 'make_developer_payload'
   get 'verify_product', to: 'home#verify_purchase_product', as: 'verify_purchase_product'
 
